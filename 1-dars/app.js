fa2e4153bbcd8cc18812d452485b12f9154c0dc2
Xudoyberdi <xudoyberdi3003@gmail.com>
2025-07-14 00:46:52 +0500

    typeScript 1-dars

---
+++ 1-dars/app.js
@@ -0,0 +1,7 @@
+"use strict";
+let fullName = 'Alex Ali';
+let isMarried = true;
+let age = 25;
+console.log(fullName);
+console.log(isMarried);
+console.log(age);
---
+++ 1-dars/app.ts
@@ -0,0 +1,7 @@
+let fullName: string = 'Alex Ali'
+let isMarried: boolean = true
+let age: number = 25
+
+console.log(fullName)
+console.log(isMarried)
+console.log(age)
