8f22378f77eb0cebd357cecb4a6d4eb89e3f5161
moliya <developper853@gmail.com>
2025-07-16 16:32:06 +0500

    TypeScript 2-Modul

---
+++ 2-Modul Murakkab Typelar/app.js
@@ -0,0 +1,4 @@
+"use strict";
+//Union Types in TypeScript
+let data = ['Hello', 42, 'World', 100];
+console.log(data);
---
+++ 2-Modul Murakkab Typelar/app.ts
@@ -0,0 +1,3 @@
+//Union Types in TypeScript
+let data: (string | number)[] = ['Hello', 42, 'World', 100]
+console.log(data)
