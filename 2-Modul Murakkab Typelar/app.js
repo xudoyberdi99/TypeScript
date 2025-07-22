1092d005ce9944a3ac7e33c5ab6224ff482f9e4e
moliya <developper853@gmail.com>
2025-07-22 17:54:23 +0500

    typeAlias

--- 2-Modul Murakkab Typelar/app.js
+++ 2-Modul Murakkab Typelar/app.js
@@ -2,3 +2,15 @@
 //Union Types in TypeScript
 let data = ['Hello', 42, 'World', 100];
 console.log(data);
+//Letiral Types in TypeScript aniq qiymatlarni saqlaydi va qayataradi
+let literalData;
+literalData = 'Hello'; // Bu to'g'ri
+let persons = [
+    { firstName: 'John', lastName: 'Doe' },
+    {
+        firstName: 'Jane',
+        lastName: 'Doe',
+        wife: { firstName: 'Alice', lastName: 'Smith' },
+    },
+];
+console.log(persons);
--- 2-Modul Murakkab Typelar/app.ts
+++ 2-Modul Murakkab Typelar/app.ts
@@ -1,3 +1,28 @@
 //Union Types in TypeScript
 let data: (string | number)[] = ['Hello', 42, 'World', 100]
 console.log(data)
+//Letiral Types in TypeScript aniq qiymatlarni saqlaydi va qayataradi
+let literalData: 'Hello' | 'World'
+literalData = 'Hello' // Bu to'g'ri
+//Literal Type - turli turdagi qiymatlarni qabul qiladi
+//Union Type - faqat aniq belgilangan qiymatlarni qabul qiladi
+
+//Type Aliases bu biz yangi type yaratishimiz mumkin
+type Person = {
+	firstName: string
+	lastName: string
+	wife?: Person
+}
+
+type Personlist = Person[]
+
+let persons: Personlist = [
+	{ firstName: 'John', lastName: 'Doe' },
+	{
+		firstName: 'Jane',
+		lastName: 'Doe',
+		wife: { firstName: 'Alice', lastName: 'Smith' },
+	},
+]
+
+console.log(persons)
